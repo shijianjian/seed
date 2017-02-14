4067cee9c5696d5a6b9c9097a3a666e96e568535
sj8716643 <sj8716643@126.com>
2017-02-14 09:20:21 +0000

    change to observables

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,80 +1,10 @@
 import { Component } from '@angular/core';
-import { MaterialService } from './common/materials.service';
-import { ContentEnum, ContentEnumDecorator, ActionEnum, ActionEnumDecorator } from './common/enums'
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html',
-    providers: [ MaterialService ]
+    templateUrl: './app.component.html'
 })
 
-@ActionEnumDecorator
-@ContentEnumDecorator
 export class AppComponent {
 
-    modalTitle = "Add new content";
-    buttonName = "+";
-    columns;
-    data = [];
-
-    actionEnum: any = ActionEnum;
-
-    constructor(
-        private _materialService: MaterialService
-    ) {
-        this.loadColumns();
-    }
-
-    deleteMessageHandler(e) {
-        if(e.action == "delete") {
-            if(this.checkDataExists(e.data.id)){
-                var i = this.findDataIndex(e.data.id);
-                this.data.splice(i,1);
-            }
-            if(e.deleteFromDB == true) {
-                console.log("Delete from db");
-            }
-        }
-    }
-
-    updateDataHandler(e){
-        if(e.needUpdateData == true) {
-            this.loadColumns();
-        }
-        if(e.newData) {
-            if(!this.checkDataExists(e.newData.id))
-                this.data.push(e.newData);
-        }
-    }
-
-    checkDataExists(id) {
-        for(var i=0; i<this.data.length; i++){
-            if(id == this.data[i].id){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    findDataIndex(id) {
-        for(var i=0; i<this.data.length; i++){
-            if(id == this.data[i].id){
-                return i;
-            }
-        }
-    }
-
-    loadColumns(){
-        this._materialService.getColumns()
-            .subscribe(data => {
-                this.columns = data;
-            });
-    }
-
-    loadData() {
-        this._materialService.getUsers()
-            .subscribe(data => {
-                this.data = data;
-            });
-    }
 }
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,53 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 
-// Does not work with system.js
-import { ModalModule } from 'ng2-bootstrap';
+// import { ModalModule } from 'ng2-bootstrap';
 
-// util imports
-import { KeyValuePipe } from './common/key-value.pipe';
-import { JsonObjectPipe } from './common/json-object.pipe';
-import { ConfirmationModalComponent } from './common/confirmationModal.component'
+import { MaterialsModule } from './materials/materials.module';
 
 import { AppComponent } from "./app.component";
-import { SearchBoxComponent } from "./searchBox/search-box.component";
-import { SearchBoxDropdownComponent } from "./searchBox/search-box-dropdown.component";
-import { SearchBoxDropdownItemComponent } from "./searchBox/search-box-dropdown-item.component";
-import { CardsComponent } from "./cards/cards.component";
-import { CardComponent } from "./cards/card.component";
-import { DataRendererModalComponent } from './dataRendererModal/data-renderer.component';
-import { FormListComponent } from './formList/form-list.component';
-import { FormItemComponent } from './formList/form-item.component';
-import { TableListComponent } from './tableList/table-list.component';
+
 
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 
 
 @NgModule({
     declarations: [
-        AppComponent,
-        SearchBoxComponent,
-        DataRendererModalComponent,
-        FormListComponent,
-        FormItemComponent,
-        TableListComponent,
-        CardsComponent,
-        CardComponent,
-        SearchBoxDropdownComponent,
-        SearchBoxDropdownItemComponent,
-        KeyValuePipe,
-        JsonObjectPipe,
-        ConfirmationModalComponent
+        AppComponent
     ],
     imports: [
-        BrowserModule, 
-        ReactiveFormsModule,
-        FormsModule, 
-        HttpModule, 
-        ModalModule.forRoot()
+        BrowserModule,
+        // ModalModule.forRoot(),
+        MaterialsModule
     ],
     bootstrap: [AppComponent]
 })
