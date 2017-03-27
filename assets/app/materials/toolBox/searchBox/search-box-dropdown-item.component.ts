b9d6c4d2fa044eeeba012fa89916acb490d97ffc
SHI Jian <305510@dom1.ad.sys>
2017-03-27 15:16:31 +0100

    fuzzy search optimazation

--- assets/app/materials/toolBox/searchBox/search-box-dropdown-item.component.ts
+++ assets/app/materials/toolBox/searchBox/search-box-dropdown-item.component.ts
@@ -1,27 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 import { MaterialsEventService } from '../../materials.event.service';
 
 @Component({
     selector: 'my-search-box-dropdown-item',
     template: `
-        <a class="col-sm-12 input-group my-search-list" *ngFor="let one of item" (click)="onClick(item)">
-            <div class="col-sm-5 input-group-addon my-search-list-item" *ngIf="one.value && one.value.toUpperCase().indexOf(target.toUpperCase())>-1 && target!='' && one.key.toUpperCase()!='ID'">{{one.key}}</div>
-            <div class="col-sm-7 form-control my-search-list-item" *ngIf="one.value && one.value.toUpperCase().indexOf(target.toUpperCase())>-1 && target!='' && one.key.toUpperCase()!='ID'"> {{one.value}}</div>
+        <a class="my-search-group" *ngFor="let one of item" (click)="onClick(item)">
+            <div *ngFor="let t of targetList" class="input-group my-search-list">
+                <div 
+                    class="input-group-addon my-search-list-item" 
+                    style="flex:5"
+                    *ngIf="one.value && one.value.toUpperCase().indexOf(t.toUpperCase())>-1 && t!='' && one.key.toUpperCase()!='ID'"> {{one.key}}</div>
+                <div 
+                    class="form-control my-search-list-item" 
+                    style="flex:7"
+                    *ngIf="one.value && one.value.toUpperCase().indexOf(t.toUpperCase())>-1 && t!='' && one.key.toUpperCase()!='ID'"> {{one.value}}</div>
+            </div>
         </a>
         `
 })
 
-export class SearchBoxDropdownItemComponent {
+export class SearchBoxDropdownItemComponent implements OnChanges {
     @Input() item;
     // reminder: target is the text typed in the searchBox
-    @Input() target;
+    @Input() target : string;
 
     data = [];
+    targetList = [];
 
     constructor(
         private _materialEventService: MaterialsEventService
     ){}
+
+    ngOnChanges() {
+        this.targetList = this.target.trim().split(" ");
+    }
 
     onClick(data) : void {
         this._materialEventService.updateData(data);
